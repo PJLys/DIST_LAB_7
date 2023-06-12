33135b56f13923282783dd4934f600eb84aaa01c
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-06-12 17:19:35 +0200

    Debug 7

--- src/main/java/dist/group2/ReplicationClient.java
+++ src/main/java/dist/group2/ReplicationClient.java
@@ -417,20 +417,20 @@ public class ReplicationClient implements Runnable{
             os_file.close();
 
             // Check if a log file has been sent
-            String log_data = (String) json.get("log_data");
-            System.out.println("Log_data for received file " + file_name + " " + log_data);
-            if (log_data != null) {
+            Object log_object = json.get("log_data");
+            if (log_object == null) {
+                // Create a new log file
+                List<Integer> replicators = new ArrayList<>();
+                replicators.add(Client.getNodeIdForIp(senderIP));
+                Logger.createLogFile(log_file_path, nodeID, replicators);
+            }
+            else {
+                String log_data = (String) log_object;
                 // Log file has been sent -> store it and update the owner
                 os_file = new FileOutputStream(log_file_path);
                 os_file.write(log_data.getBytes());
                 os_file.close();
                 Logger.setOwner(log_file_path, nodeID);
-            }
-            else {
-                // Create a new log file
-                List<Integer> replicators = new ArrayList<>();
-                replicators.add(Client.getNodeIdForIp(senderIP));
-                Logger.createLogFile(log_file_path, nodeID, replicators);
             }
         } else if (Objects.equals(extra_message, "ENTRY_MODIFY")) {
             // Store the replicated file
