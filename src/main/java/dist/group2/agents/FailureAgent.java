c77ef1ed773700081896f782f09c29eb58b2ef44
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-06-19 10:49:11 +0200

    Some new debugs 17

--- src/main/java/dist/group2/agents/FailureAgent.java
+++ src/main/java/dist/group2/agents/FailureAgent.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Objects;
 
 
-public class FailureAgent implements Serializable {
+public class FailureAgent implements Serializable, Runnable{
 
     private final int failingNodeId;
     /**
@@ -38,6 +38,7 @@ public class FailureAgent implements Serializable {
     }
 
 
+    @Override
     public void run() {
         // Check if the local files are owned by the failing node, and if so, send them to their new owner. The failing node does not have to do this.
         if (DiscoveryClient.getCurrentID() != this.failingNodeId) {
